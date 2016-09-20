b6a9f1a5c54890f7cfa46cc83973eaa87e41c91e
Michael Perkhofer <michael@perkhofer.at>
2016-09-20 23:01:45 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
